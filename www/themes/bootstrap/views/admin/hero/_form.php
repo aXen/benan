38c463fb939b2a45b6706b16ac8cc621440bb249
aXen <BusyAnt@qq.com>
2014-05-29 01:33:16 +0800

    排版

--- www/themes/bootstrap/views/admin/hero/_form.php
+++ www/themes/bootstrap/views/admin/hero/_form.php
@@ -1,65 +1,72 @@
 <?php
-$cs=Yii::app()->clientScript;
+$cs = Yii::app()->clientScript;
 $baseUrl = Yii::app()->baseUrl;
 
 // select
-$cs->registerScriptFile($baseUrl.'/widgets/bootstrap-select/bootstrap-select.js', CClientScript::POS_END, array('charset'=>'UTF-8'));
+$cs->registerScriptFile($baseUrl . '/widgets/bootstrap-select/bootstrap-select.js', CClientScript::POS_END, array('charset' => 'UTF-8'));
 //$cs->registerScriptFile($baseUrl.'/widgets/bootstrap-select/bootstrap-select.js', CClientScript::POS_END, array('charset'=>'UTF-8'));
-$cs->registerCssFile($baseUrl.'/widgets/bootstrap-select/bootstrap-select.css');
+$cs->registerCssFile($baseUrl . '/widgets/bootstrap-select/bootstrap-select.css');
 ?>
 
-<?php $form=$this->beginWidget('bootstrap.widgets.TbActiveForm',array(
-	'id'=>'hero-form',
-	'enableAjaxValidation'=>false,
+<?php $form = $this->beginWidget('bootstrap.widgets.TbActiveForm', array(
+    'id' => 'hero-form',
+    'enableAjaxValidation' => false,
+    'type' => 'horizontal'
 )); ?>
 
-	<p class="help-block">带 <span class="required">*</span> 是必填的。</p>
+<hr/>
 
-	<?php echo $form->errorSummary($model); ?>
+<p class="form-help text-right text-info">带 <span class="required">*</span> 是必填的。</p>
 
-	<?php echo $form->textFieldRow($model,'title',array('class'=>'span5','maxlength'=>200, 'placeholder'=>'请输入'.$model->getAttributeLabel('title'))); ?>
+<?php echo $form->errorSummary($model); ?>
+
+<?php echo $form->textFieldRow($model, 'title', array('class' => 'span5', 'maxlength' => 200, 'placeholder' => '请输入' . $model->getAttributeLabel('title'))); ?>
 
 <div style="display:none;">
-	<?php echo $form->textFieldRow($model,'picture',array('class'=>'span5','maxlength'=>128, 'placeholder'=>'请输入'.$model->getAttributeLabel('picture'))); ?>
+    <?php echo $form->textFieldRow($model, 'picture', array('class' => 'span5', 'maxlength' => 128, 'placeholder' => '请输入' . $model->getAttributeLabel('picture'))); ?>
 </div>
-   
-    <?php
-    $this->widget('application.extensions.dropzone.EDropzone', array(
-        'model' => $model,
-        'attribute' => 'picture',
-        'url' => $this->createUrl('file/uploadpicture'),
-        'mimeTypes' => array('image/jpeg', 'image/png'),
-        'onSuccess' => 'AfterUpload(arguments);',
-        'options' => array(),
-    ));
-    ?>
-    <script type="text/javascript">
-        // 保存图片名称
-        function AfterUpload(args){
-            $("input[name='Hero[picture]']").val(args[1]);
-        }
-    </script>
 
-	<?php echo $form->textFieldRow($model,'description',array('class'=>'span5','maxlength'=>2000, 'placeholder'=>'请输入'.$model->getAttributeLabel('description'))); ?>
+<div class="control-group ">
+    <label class="control-label required">上传图片<span class="required">*</span></label>
+    <div class="controls">
+        <?php
+        $this->widget('application.extensions.dropzone.EDropzone', array(
+            'model' => $model,
+            'attribute' => 'picture',
+            'url' => $this->createUrl('file/uploadpicture'),
+            'mimeTypes' => array('image/jpeg', 'image/png'),
+            'onSuccess' => 'AfterUpload(arguments);',
+            'options' => array(),
+        ));
+        ?>
+        <script type="text/javascript">
+            // 保存图片名称
+            function AfterUpload(args) {
+                $("input[name='Hero[picture]']").val(args[1]);
+            }
+        </script>
+    </div>
+</div>
+<?php echo $form->textFieldRow($model, 'description', array('class' => 'span5', 'maxlength' => 2000, 'placeholder' => '请输入' . $model->getAttributeLabel('description'))); ?>
 
-	<?php echo $form->textFieldRow($model,'href',array('class'=>'span5','maxlength'=>128, 'placeholder'=>'请输入'.$model->getAttributeLabel('href'))); ?>
+<?php echo $form->textFieldRow($model, 'href', array('class' => 'span5', 'maxlength' => 128, 'placeholder' => '请输入' . $model->getAttributeLabel('href'))); ?>
 
 <!--	--><?php //echo $form->textFieldRow($model,'hit',array('class'=>'span5', 'placeholder'=>'请输入'.$model->getAttributeLabel('hit'))); ?>
 
-    <?php echo $form->dropDownListRow($model,'rank',array('1'=>'1', '2'=>'2', '3'=>'3', '4'=>'4', '5'=>'5'), array('class'=>'span5 selectpicker','maxlength'=>16, 'placeholder'=>'请输入'.$model->getAttributeLabel('rank'))); ?>
+<?php echo $form->dropDownListRow($model, 'rank', array('1' => '1', '2' => '2', '3' => '3', '4' => '4', '5' => '5'), array('class' => 'span5 selectpicker', 'maxlength' => 16, 'placeholder' => '请输入' . $model->getAttributeLabel('rank'))); ?>
 
-	<div class="form-actions">
-		<?php $this->widget('bootstrap.widgets.TbButton', array(
-			'buttonType'=>'submit',
-			'type'=>'primary',
-			'label'=>$model->isNewRecord ? '创建' : '保存',
-		)); ?>
-	</div>
+<div class="form-actions">
+    <?php $this->widget('bootstrap.widgets.TbButton', array(
+        'buttonType' => 'submit',
+        'type' => 'primary',
+        'label' => $model->isNewRecord ? '创建' : '保存',
+    )); ?>
+</div>
 
 <?php $this->endWidget(); ?>
 
 <script type="text/javascript">
-    $(function(){
+    $(function () {
         $('.selectpicker').selectpicker();
     });
 </script>
--- www/themes/bootstrap/views/admin/layouts/_footer.php
+++ www/themes/bootstrap/views/admin/layouts/_footer.php
@@ -1,3 +1,4 @@
+<hr />
 <div id="footer">
     <dl>
         <dt></dt>
